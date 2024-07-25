ce6ce4018b269b43777dd8f79ad85fdec0a16677
prajaktakadu11 <kaduprajakta98@gmail.com>
2024-07-26 01:32:43 +0530

    Add files via upload

---
+++ Credit card transaction data.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE Credit_card_db;
+
+use Credit_card_db;
+
+CREATE TABLE Credit_details (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date date,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Vol INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+
+CREATE TABLE Cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+
+select count(client_Num) from credit_details;
+
+UPDATE credit_details
+SET Week_Start_Date = STR_TO_DATE(Week_Start_Date, '%d-%m-%Y');
+
+ 
+
+
